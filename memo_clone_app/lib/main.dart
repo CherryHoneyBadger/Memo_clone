71c8ead00b7a75f0dcbc14da3fbeb07fdf84af52
CherryHoneyBadger <mcdavemu@gmail.com>
2019-09-09 15:54:27 +0900

    2019/09/09 v0.2

--- memo_clone_app/lib/main.dart
+++ memo_clone_app/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui' as prefix0;
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -7,12 +9,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: folder_page()
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+            primaryColor: Colors.white,
+            accentColor: Colors.white,
+            primaryColorBrightness: Brightness.light),
+        home: folder_page());
   }
 }
 
@@ -25,9 +27,25 @@ class _folder_pageState extends State<folder_page> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(actions: <Widget>[
+        
+      ],),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(
+          '폴더',
+          style: TextStyle(
+              fontSize: 35, fontFamily: "Roboto", fontWeight: FontWeight.w700),
+        ),
+
+      ),
       
-    )
       
+      floatingActionButton: FloatingActionButton(
+        //버튼을 누를시 새로운 폴더를 생성
+        onPressed: () {},
+        child: Icon(Icons.create_new_folder),
+      ),
     );
   }
 }
