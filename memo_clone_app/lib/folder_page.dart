bd3bdef8a02079bf58b3a35332827c73cd6c2df6
CherryHoneyBadger <mcdavemu@gmail.com>
2019-09-17 22:39:04 +0900

    2019/07/17 v0.5

--- memo_clone_app/lib/folder_page.dart
+++ memo_clone_app/lib/folder_page.dart
@@ -10,20 +10,48 @@ class _Folder_PageState extends State<Folder_Page> {
   final String foldertitle = '폴더';
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-        child: CustomScrollView(
-      slivers: <Widget>[
-        CupertinoSliverNavigationBar(
-          largeTitle: Text('폴더',
-              style: TextStyle(
-                  fontStyle: FontStyle.normal, fontWeight: FontWeight.bold)),
-          trailing: Text('편집',
-              style: TextStyle(
-                  color: Colors.blueGrey,
-                  fontStyle: FontStyle.normal,
-                  fontWeight: FontWeight.bold)),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.edit),
+            color: Colors.green,
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          )
+        ],
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    '폴더',
+                    style: TextStyle(fontSize: 45, fontWeight: FontWeight.bold),
+                  ),
+                  Text('리스트 뷰를 사용해서 폴더 리스트 만들기')
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          Icons.add_circle,
+          color: Colors.green,
+          size: 50,
         ),
-      ],
-    ));
+        onPressed: () {
+          Navigator.pop(context);
+        },
+      ),
+    );
   }
 }
