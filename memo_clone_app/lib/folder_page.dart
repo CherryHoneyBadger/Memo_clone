c1764172a2085a6e40690424c9f50060dbd19870
CherryHoneyBadger <mcdavemu@gmail.com>
2019-09-18 22:01:46 +0900

    2019/09/18 v0.6

--- memo_clone_app/lib/folder_page.dart
+++ memo_clone_app/lib/folder_page.dart
@@ -7,51 +7,63 @@ class Folder_Page extends StatefulWidget {
 }
 
 class _Folder_PageState extends State<Folder_Page> {
-  final String foldertitle = '폴더';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.edit),
-            color: Colors.green,
+    return Row(
+      children: <Widget>[
+        Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            actions: <Widget>[
+              IconButton(
+                icon: const Icon(Icons.edit),
+                color: Colors.green,
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              )
+            ],
+          ),
+          body: Column(
+            children: <Widget>[
+              Container(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        '폴더',
+                        style: TextStyle(fontSize: 45, fontWeight: FontWeight.bold),
+                      ),
+                      ListView(
+                        padding: const EdgeInsets.all(8),
+                        children: <Widget>[
+                          Container(
+                            height: 20,
+                            color: Colors.green,
+                            child: const Text('메모'),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+          floatingActionButton: FloatingActionButton(
+            child: Icon(
+              Icons.add_circle,
+              color: Colors.green,
+              size: 50,
+            ),
             onPressed: () {
               Navigator.pop(context);
             },
-          )
-        ],
-      ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    '폴더',
-                    style: TextStyle(fontSize: 45, fontWeight: FontWeight.bold),
-                  ),
-                  Text('리스트 뷰를 사용해서 폴더 리스트 만들기')
-                ],
-              ),
-            ),
-          )
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(
-          Icons.add_circle,
-          color: Colors.green,
-          size: 50,
+          ),
         ),
-        onPressed: () {
-          Navigator.pop(context);
-        },
-      ),
+      ],
     );
   }
 }
